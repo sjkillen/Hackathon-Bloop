e7fec992b993f4bd6ece2ab9e8aea11fab55d2fc
Tobais-Renwick <renwickt@mymacewan.ca>
2017-01-28 14:57:12 -0700

    fixes typo orchestrator

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const path = require("path")
+
 
 dotenv.config()
 
-
+pathV = path.resolve(__dirname, "orchestrator/dist")
+console.log(pathV)
 app = express()
-app.use(express.static("orchesrtator/dist"))
+app.use("/", express.static(pathV))
 app.listen(process.env.PORT)
 console.log(process.env.PORT)
