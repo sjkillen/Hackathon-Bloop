8f2133d47675653b5e60a972b94edcbc485e0b20
skwidz <christian.c.ellinger@gmail.com>
2017-01-28 17:42:40 -0700

    ..and the young boy did his first bloop, and there was much rejoycing

--- BloopAndroid/app/src/main/java/io/everybodybloops/bloop/bloop/Connected.java
+++ BloopAndroid/app/src/main/java/io/everybodybloops/bloop/bloop/Connected.java
@@ -8,10 +8,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import io.everybodybloops.bloop.bloop.MediaPlayerController;
 
 import io.everybodybloops.bloop.bloop.MediaPlayerController;
 
 public class Connected extends AppCompatActivity {
+    String soundurl = "http://everybody-bloops.herokuapp.com/sound/bloop.mp3";
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,8 @@ public class Connected extends AppCompatActivity {
         setContentView(R.layout.activity_connected);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+       final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.bloop);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,12 +34,21 @@ public class Connected extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Button playbutton = (Button) findViewById(R.id.playButton);
+        playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+//
+                mp.start();
 
-
+            }
+        });
     }
     // things to run when the activity pauses.
     protected void onPause(){
-
+        mp.release();
+        mp = null;
     }
+    
 }
--- BloopAndroid/app/src/main/java/io/everybodybloops/bloop/bloop/MediaPlayerController.java
+++ BloopAndroid/app/src/main/java/io/everybodybloops/bloop/bloop/MediaPlayerController.java
@@ -3,6 +3,8 @@ package io.everybodybloops.bloop.bloop;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -12,7 +14,7 @@ import android.widget.Toast;
 
 public class MediaPlayerController extends Connected {
 
-    Context context = super.getApplicationContext();
+
     MediaPlayer mediaPlayer;
 
     //constructor for the media player class
@@ -20,27 +22,27 @@ public class MediaPlayerController extends Connected {
         MediaPlayer mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         // Try and get the data source from the url passed
+
         try {
             mediaPlayer.setDataSource(url);
-        } catch (java.io.IOException e) {
-            // the mediaplayer cannot resolve the Url
-            CharSequence text = "Url could not be resolved";
-            Toast toast = Toast.makeText(context, text,Toast.LENGTH_SHORT);
-            toast.show();
-            // will need to break out of the
-        }
-       try{ // try and
+
+        System.out.println("datasource found" + url);
            mediaPlayer.prepare();
        } catch (java.io.IOException e){
            // the file could not be prepared
            CharSequence text = "File could not be prepared";
-           Toast toast = Toast.makeText(context, text,Toast.LENGTH_SHORT);
-           toast.show();
+            System.out.println("prepare failed");
+          // Toast toast = Toast.makeText(context, text,Toast.LENGTH_SHORT);
+           //toast.show();
+
        }
-    }
+        System.out.println("player prepared");
+     }
 
     public void playSound(){ // Needs to take a time
         mediaPlayer.start();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 
     // method to close the MediaPlayer instance
